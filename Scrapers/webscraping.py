b17579983653a31325dae3fc6a6bdc6f64ab32fb
SuperSlowMotionNinja <44821714+SuperSlowMotionNinja@users.noreply.github.com>
2019-02-17 11:26:04 -0500

    Update webscraping.py

--- Scrapers/webscraping.py
+++ Scrapers/webscraping.py
@@ -3,23 +3,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import json
 
-def getKeyWords(link, key_terms):
-    browser = webdriver.Chrome(executable_path='C:/Users/Soumen/Desktop/Programming/Selenium Drivers/chromedriver.exe')
+def getKeyWords(link,currentID):
+    key_terms = ["python", "java", "javascript", " c ", "c#", "c++", "ruby", "html", "html5", "maya", "sql", "mysql", "nosql", "arduino", "mongodb", "angular", "rails", "react", "django", ".net", "node", "assembly"]
+    browser = webdriver.Chrome(executable_path='/Users/ericpham/Downloads/chromedriver')
 
-    browser.get(job.get_attribute("href"))
+    browser.get(link)
     timeout = 20
     try:
-        WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='jobsearch-JobComponent-description icl-u-xs-mt--md']")))
+        WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='jobsearch-JobComponent icl-u-xs-mt--sm jobsearch-JobComponent-bottomDivider']")))
     except TimeoutException:
         print("Timed out waiting for page to load")
         browser.quit()
         return []
+
+    data = {}
+    title = browser.find_element_by_xpath("/html/body/div[1]/div[3]/div[3]/div/div/div[1]/div[1]/div[1]/h3").text
+    company = browser.find_element_by_xpath("/html/body/div[1]/div[3]/div[3]/div/div/div[1]/div[1]/div[1]/div[1]/div/div[1]").text
+    data[currentID+1] = [title,company,link,[]]
+    currentID+= 1
+
     # search for key words
-    word_elements = browser.find_elements_by_xpath("//p")
-    words = ""
-    for x in word_elements:
-        words += " "+x.text
+    words = browser.find_element_by_xpath("/html/body/div[1]/div[3]/div[3]/div/div/div[1]/div[1]").text
     # search through description for key words
     keywords = []
     for i in range(len(key_terms)):
@@ -30,47 +36,7 @@ def getKeyWords(link, key_terms):
             if key_terms[i] in words.lower():
                 keywords.append(key_terms[i])
     browser.quit()
-    return keywords
+    data[title][3] = keywords
+    # ID: [Job Title, Company Name, [Keywords]]
 
-key_terms = ["python", "java", "javascript", " c ", " c,", "/c", "c/", "c#", "c++", "ruby", "html", "html5", "maya", "sql", "mysql", "nosql", "arduino", "mongodb", "angular", "rails", "react", "django", ".net", "node", "assembly"]
-
-# open chrome incognito
-option = webdriver.ChromeOptions()
-#option.add_argument(" — incognito")
-
-# new instance of chrome
-"""
-change below
-"""
-browser = webdriver.Chrome(executable_path='C:/Users/Soumen/Desktop/Programming/Selenium Drivers/chromedriver.exe')
-
-# making a request:
-"""
-change below
-"""
-browser.get("https://ca.indeed.com/jobs?q=software+developer&l=Ottawa%2C+ON")
-# Wait 20 seconds for page to load
-timeout = 20
-try:
-    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='jobsearch-SerpJobCard row result clickcard']")))
-except TimeoutException:
-    print("Timed out waiting for page to load")
-    browser.quit()
-
-# making a response:
-
-#titles and links
-# find_elements_by_xpath returns an array of selenium objects.
-title_elements = browser.find_elements_by_xpath("//a[@class='jobtitle turnstileLink']")
-#company_elements = browser.find_elements_by_xpath("//a[@class='turnstileLink']")
-company_elements = browser.find_elements_by_xpath("//span[@class='company']")
-companies = [x.text for x in company_elements]
-counter = 0
-jobInfo = {} # title: [company, link, [keywords]]
-for job in title_elements:
-    jobInfo[job.text] = [companies[counter], job.get_attribute("href"), getKeyWords(job.get_attribute("href"),key_terms)]
-    counter += 1
-browser.quit()
-
-for job in jobInfo.keys():
-    print("Job: "+job+"\nCompany: ",jobInfo[job][0],"\nKeywords: ",jobInfo[job][2])
+    return data
