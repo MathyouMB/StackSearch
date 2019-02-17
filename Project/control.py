32fe5a525d78effaaae335214339f79bd4f4272c
Matthew M-B <matthewmacraebovell@bell.net>
2019-02-17 11:25:14 -0500

    ok

--- Project/control.py
+++ Project/control.py
@@ -23,7 +23,7 @@ def result():
 	input.append(item2)
 	input.append(item3)
 	
-	f = open('formattedData.json')
+	f = open('data.json')
 	
 	
 	data = json.load(f)
@@ -34,15 +34,15 @@ def result():
 #add all jobs
 	for(k,v) in data.items():
 		job = [] #that job
-		job.append(k) #title 0
+		job.append(v[0]) #title 0
 		job.append(v[1]) #link 1
-		job.append(v[0]) #company 2
-		job.append(v[2]) #list of tags 3
+		job.append(v[2]) #company 2
+		job.append(v[3]) #list of tags 3
 		points = 0
-		for i in range(0,len(v[2])):
+		for i in range(0,len(v[3])):
 			for j in range(0,len(input)):
-				print(input[j]+":"+v[2][i])
-				if input[j].lower()==v[2][i]:
+				print(input[j]+":"+v[3][i])
+				if input[j].lower()==v[3][i]:
 					print("+1")
 					points += 1
 		job.append(points)#relevant 4
@@ -50,6 +50,7 @@ def result():
 	
 	for x in range(len(jobs)-1,0,-1):
 		for y in range(x):
+			print()
 			if jobs[y][4]>jobs[y+1][4]:
 				temp = jobs[y]
 				jobs[y] = jobs[y+1]
